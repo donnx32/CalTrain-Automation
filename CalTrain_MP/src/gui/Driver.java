eb509da6a61bd9a2262efc787606d1e637773794
UmahasSaExMo <34360959+UmahasSaExMo@users.noreply.github.com>
2018-07-27 09:49:32 +0800

    Add dot

--- CalTrain_MP/src/gui/Driver.java
+++ CalTrain_MP/src/gui/Driver.java
@@ -4,6 +4,6 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		new SelectFrame();
-		System.out.println("Driver is currently running... ");
+		System.out.println("Driver is currently running....");
 	}
 }
